389439cd7c9caa1390e180c40af38b9c460a1fd6
tunatuna123 <tuna200538@gmail.com>
2024-02-12 22:03:38 +0900

    prettier

--- server.js
+++ server.js
@@ -1,91 +1,94 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const Product = require('./models/productModel')
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/productModel");
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.send("hello world")
-})
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
-app.get('/blog', (req, res)=> {
-    res.send('Hello blog')
-})
+app.get("/blog", (req, res) => {
+  res.send("Hello blog");
+});
 
 // Create
-app.get('/products', async(req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 // Read List
-app.get('/products/:id', async(req, res) => {
-    try {
-        const {id} = req.params;
-        const product = await Product.findById(id);
-        res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
+app.get("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 // Read
-app.post('/product', async(req,res) => {
-    try{
-        const product = await Product.create(req.body)
-        res.status(200).json(product)
-
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({message: error.message})
-    }
-})
+app.post("/product", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
 
 // Update
-app.put('/products/:id', async(req, res) => {
-    try {
-        const {id} = req.params;
-        const product = await Product.findByIdAndUpdate(id, req.body);
-        if(!product){
-            return res.status(404).json({message: `cannot find any product with ID ${id}`})
-        }
-        const UpdatedProduct = await Product.findById(id);
-        res.status(200).json(UpdatedProduct);
-
-    } catch (error) {
-        res.status(500).json({message: error.message})
+app.put("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any product with ID ${id}` });
     }
-})
+    const UpdatedProduct = await Product.findById(id);
+    res.status(200).json(UpdatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 // Delete
-app.delete('/products/:id', async(req, res) => {
-    try {
-        const {id} = req.params;
-        const product = await Product.findByIdAndUpdate(id, req.body);
-        if(!product){
-            return res.status(404).json({message: `cannot find any product with ID ${id}`})
-        }
-        res.status(200).json(product);
-
-    } catch (error) {
-        res.status(500).json({message: error.message})
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any product with ID ${id}` });
     }
-})
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-
-mongoose.set('strictQuery', false)
+mongoose.set("strictQuery", false);
 mongoose
-.connect('mongodb+srv://admin:admin@wingsurvival.udq99yc.mongodb.net/?retryWrites=true&w=majority')
-.then(()=> {
-    app.listen(3000, ()=> {
-        console.log('Node API app is running on port 3000')
+  .connect(
+    "mongodb+srv://admin:admin@wingsurvival.udq99yc.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Node API app is running on port 3000");
     });
-}).catch((error) => {
-    console.log('error')
-})
+  })
+  .catch((error) => {
+    console.log("error");
+  });
